5c5805312ab2113fd2e61705a1cce88c771b4f54
xf11id operator <xf11id@xf11id.nsls2.local>
2021-10-27 10:41:21 -0400

    Patch out wait for connection

--- startup/00-base.py
+++ startup/00-base.py
@@ -50,9 +50,50 @@ runengine_metadata_dir = appdirs.user_data_dir(appname="bluesky") / Path("runeng
 # PersistentDict will create the directory if it does not exist
 RE.md = PersistentDict(runengine_metadata_dir)
 
+# send ophyd debug log to the console
+# import logging
+#logging.getLogger('ophyd').setLevel('DEBUG')
+#console_handler = logging.StreamHandler()
+#console_handler.setLevel("DEBUG")
+#logging.getLogger('ophyd').addHandler(console_handler)
 
-# Turn down super-verbose logging for caproto
-import logging
-# logging.getLogger('caproto').setLevel('ERROR')
-# logging.getLogger('caproto.ch').setLevel('ERROR')
-logging.getLogger('ophyd').setLevel('WARNING')
+###############################################################################
+# TODO: remove this block once https://github.com/bluesky/ophyd/pull/959 is
+# merged/released.
+from datetime import datetime
+from ophyd.signal import EpicsSignalBase, EpicsSignal, DEFAULT_CONNECTION_TIMEOUT
+
+def print_now():
+    return datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S.%f')
+
+def wait_for_connection_base(self, timeout=DEFAULT_CONNECTION_TIMEOUT):
+    '''Wait for the underlying signals to initialize or connect'''
+    if timeout is DEFAULT_CONNECTION_TIMEOUT:
+        timeout = self.connection_timeout
+    # print(f'{print_now()}: waiting for {self.name} to connect within {timeout:.4f} s...')
+    start = time.time()
+    try:
+        self._ensure_connected(self._read_pv, timeout=timeout)
+        # print(f'{print_now()}: waited for {self.name} to connect for {time.time() - start:.4f} s.')
+    except TimeoutError:
+        if self._destroyed:
+            raise DestroyedError('Signal has been destroyed')
+        raise
+
+def wait_for_connection(self, timeout=DEFAULT_CONNECTION_TIMEOUT):
+    '''Wait for the underlying signals to initialize or connect'''
+    if timeout is DEFAULT_CONNECTION_TIMEOUT:
+        timeout = self.connection_timeout
+    # print(f'{print_now()}: waiting for {self.name} to connect within {timeout:.4f} s...')
+    start = time.time()
+    self._ensure_connected(self._read_pv, self._write_pv, timeout=timeout)
+    # print(f'{print_now()}: waited for {self.name} to connect for {time.time() - start:.4f} s.')
+
+EpicsSignalBase.wait_for_connection = wait_for_connection_base
+EpicsSignal.wait_for_connection = wait_for_connection
+###############################################################################
+
+from ophyd.signal import EpicsSignalBase
+# EpicsSignalBase.set_default_timeout(timeout=10, connection_timeout=10)  # old style
+EpicsSignalBase.set_defaults(timeout=10, connection_timeout=10)  # new style
+
